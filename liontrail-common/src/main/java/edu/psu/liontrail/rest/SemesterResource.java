0b53572d2b034a92664e0e9014fdfb7e55e3d6f0
Mike Z <mez5001@psu.edu>
2017-09-10 21:15:18 -0400

    Adding update and create methods for SemesterResource RESR endpoint

--- liontrail-common/src/main/java/edu/psu/liontrail/rest/SemesterResource.java
+++ liontrail-common/src/main/java/edu/psu/liontrail/rest/SemesterResource.java
@@ -1,6 +1,8 @@
 package edu.psu.liontrail.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -8,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import edu.psu.liontrail.enumeration.SemesterSeason;
+import edu.psu.liontrail.model.Semester;
 
 @Path(SemesterResource.PATH)
 public interface SemesterResource {
@@ -28,5 +31,13 @@ public interface SemesterResource {
   @Path("year/{year}/season/{season}")
   @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
   Response getSemesterByYearAndSeason(@PathParam("year") int year, @PathParam("season") SemesterSeason season);
+  
+  @PUT
+  @Path("id/{id}")
+  Response updateSemester(@PathParam("id") int id, Semester semester);
+  
+  @POST
+  @Path("id/{id}")
+  Response createSemester(@PathParam("id") int id, Semester semester);
 
 }
--- liontrail-web/src/main/java/edu/psu/liontrail/rest/SemesterResourceImpl.java
+++ liontrail-web/src/main/java/edu/psu/liontrail/rest/SemesterResourceImpl.java
@@ -5,35 +5,81 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import edu.psu.liontrail.enumeration.SemesterSeason;
+import edu.psu.liontrail.exception.ErrorMessage;
+import edu.psu.liontrail.exception.ValidationException;
 import edu.psu.liontrail.model.Semester;
-import edu.psu.liontrail.rest.SemesterResource;
-import edu.psu.liontrail.store.SemesterStore;
+import edu.psu.liontrail.service.SemesterService;
 
 public class SemesterResourceImpl implements SemesterResource {
   
   @Inject
-  SemesterStore semesterStore;
+  SemesterService service;
 
   @Override
   public Response getSemesterById(int id) {
-    Semester semester = semesterStore.getSemesterById(id);
-    
+    Semester semester = service.getSemester(id);
     return Response.ok().entity(semester).build();
   }
 
   @Override
   public Response getAllSemesters() {
-    List<Semester> semesters = semesterStore.getAllSemesters();
+    List<Semester> semesters = service.getAllSemesters();
     GenericEntity<List<Semester>> list = new GenericEntity<List<Semester>>(semesters){};
     return Response.ok().entity(list).build();
   }
 
   @Override
   public Response getSemesterByYearAndSeason(int year, SemesterSeason season) {
-    Semester semester = semesterStore.getSemesterBySeasonAndYear(season, year);
-    return Response.ok().entity(semester).build();
+    try {
+      Semester semester = service.getSemester(season, year);
+      return Response.ok().entity(semester).build();
+    } catch (ValidationException e) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, e.getMessages());
+      return em.toResponse();
+    }
+  }
+
+  @Override
+  public Response updateSemester(int id, Semester semester) {
+    if (semester == null) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, "smester payload was null");
+      return em.toResponse();
+    }
+    if (semester.getId() != id) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, "id of semester does not path value");
+      return em.toResponse();
+    }
+    
+    try {
+      service.updateSemester(semester);
+      return Response.accepted().build();
+    } catch (ValidationException e) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, e.getMessages());
+      return em.toResponse();
+    }
+  }
+
+  @Override
+  public Response createSemester(int id, Semester semester) {
+    if (semester == null) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, "smester payload was null");
+      return em.toResponse();
+    }
+    if (semester.getId() != id) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, "id of semester does not path value");
+      return em.toResponse();
+    }
+    
+    try {
+      service.addSemester(semester);
+      return Response.accepted().build();
+    } catch (ValidationException e) {
+      ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST, e.getMessages());
+      return em.toResponse();
+    }
   }
 
 }
