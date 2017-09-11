479d0dfb49e4bf6009c693608be3d2d596466aca
Mike Z <mez5001@psu.edu>
2017-09-10 20:07:42 -0400

    Updating additional tests to SemesterServiceTest

--- liontrail-web/src/test/java/edu/psu/liontrail/service/SemesterServiceTest.java
+++ liontrail-web/src/test/java/edu/psu/liontrail/service/SemesterServiceTest.java
@@ -35,17 +35,21 @@ public class SemesterServiceTest {
   }
   
   @Test
-  public void testAddSemesterWithIdValidation() {
+  public void testAddSemesterValidation() {
     Semester semester = createDefaultSemester();
     semester.setId(5);
+    boolean exceptionCaught = false;
     
     //Check that id cannot be null
     try {
       service.addSemester(semester);
+      exceptionCaught = false;
     } catch (ValidationException e) {
+      exceptionCaught = true;
       Assert.assertEquals(1, e.getMessages().size());
       Assert.assertEquals("Cannot specify semester id when creating new semester", e.getMessages().get(0));
     }
+    Assert.assertTrue(exceptionCaught);
     
     //Check that semester already exists throws an error
     semester.setId(0);
@@ -53,11 +57,71 @@ public class SemesterServiceTest {
       .thenReturn(createDefaultSemester());
     try {
       service.addSemester(semester);
+      exceptionCaught = false;
     } catch (ValidationException e) {
+      exceptionCaught = true;
       Assert.assertEquals(1, e.getMessages().size());
       Assert.assertEquals("Semester already exists for Season: SPRING and Year: 2017", e.getMessages().get(0));
     }
+    Assert.assertTrue(exceptionCaught);
     
+    //Test that no errors 
+    Mockito.when(store.getSemesterBySeasonAndYear(Mockito.any(SemesterSeason.class), Mockito.anyInt()))
+      .thenReturn(null);
+    try {
+      service.addSemester(semester);
+    } catch (ValidationException e) {
+      Assert.fail("Exception caught when not expected: "+e.getMessage());
+    }
+  }
+  
+  @Test
+  public void testUpdateSemester() {
+    Semester semester = createDefaultSemester();
+    boolean exceptionCaught = false;
+    int semesterId = semester.getId();
+    
+    //Verify update with invalid id throws an error
+    semester.setId(0);
+    try {
+      service.updateSemester(semester);
+      exceptionCaught = false;
+    } catch (ValidationException e) {
+      exceptionCaught = true;
+      Assert.assertEquals(1, e.getMessages().size());
+      Assert.assertEquals("Semester id must be set for update", e.getMessages().get(0));
+    }
+    Assert.assertTrue(exceptionCaught);
+    
+    //Verify that updating semester that does not exist throws exception
+    Mockito.when(store.getSemesterById(semesterId)).thenReturn(null);
+    semester.setId(semesterId);
+    try {
+      service.updateSemester(semester);
+      exceptionCaught = false;
+    } catch(ValidationException e) {
+      exceptionCaught = true;
+      Assert.assertEquals(1, e.getMessages().size());
+      Assert.assertEquals("Cannot update semester since it does not already exist", e.getMessages().get(0));
+    }
+    Assert.assertTrue(exceptionCaught);
+    
+    //Verify that updating semester with existing season/year throws Exception
+    Mockito.when(store.getSemesterById(semesterId))
+      .thenReturn(createDefaultSemester());
+    Semester otherSemester = createDefaultSemester();
+    otherSemester.setId(semesterId+1);
+    Mockito.when(store.getSemesterBySeasonAndYear(semester.getSeason(), semester.getYear()))
+      .thenReturn(otherSemester);
+    try {
+      service.updateSemester(semester);
+      exceptionCaught = false;
+    } catch (ValidationException e) {
+      exceptionCaught = true;
+      Assert.assertEquals(1, e.getMessages().size());
+      Assert.assertEquals("Semester already exists for Season: SPRING and Year: 2017", e.getMessages().get(0));
+    }
+    Assert.assertTrue(exceptionCaught);
   }
   
   private static Semester createDefaultSemester() {
