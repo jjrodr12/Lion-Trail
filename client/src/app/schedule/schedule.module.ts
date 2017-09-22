2814de4672fda0e3a4ad66a01e54488e249c0d8c
Steve Sellers <ssellers99@gmail.com>
2017-09-22 14:04:11 -0400

    Added bootstrap accordion to schedule component for semester selection

--- client/src/app/schedule/schedule.module.ts
+++ client/src/app/schedule/schedule.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
@@ -8,6 +9,7 @@ import { ScheduleService } from './schedule.service';
 @NgModule({
   imports: [
     CommonModule,
+    NgbModule,
     ScheduleRoutingModule
   ],
   declarations: [
