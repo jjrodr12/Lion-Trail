ae8e9431176aefa52c285a43db9b37c054402d72
cegb001 <uadeeb@gmail.com>
2017-10-11 12:56:08 -0400

    sprint 2

---
+++ liontrail-web/src/main/java/edu/psu/liontrail/service/GradeService.java
@@ -0,0 +1,148 @@
+package edu.psu.liontrail.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import edu.psu.liontrail.enumeration.SemesterSeason;
+import edu.psu.liontrail.exception.ValidationException;
+import edu.psu.liontrail.model.Semester;
+import edu.psu.liontrail.store.GradeStore;
+import edu.psu.liontrail.util.ValidationUtil;
+
+@Stateless
+public class GradeService {
+  
+  @Inject
+  GradeStore store;
+  
+  
+  
+  public Semester getSemester(int id) {
+    return store.getSemesterById(id);
+  }
+  
+  public List<Semester> getAllSemesters() {
+    return store.getAllSemesters();
+  }
+  
+  public Semester getSemester(SemesterSeason season, Integer year) throws ValidationException {
+    ValidationException exception = new ValidationException();
+    if (season == null) {
+      exception.addMessage("season cannot be null");
+    }
+    if (year == null) {
+      exception.addMessage("year cannot be null");
+    }
+    if (exception.hasErrors()) {
+      throw exception;
+    }
+    return store.getSemesterBySeasonAndYear(season, year);
+  }
+  
+  public void addSemester(Semester semester) throws ValidationException {
+    validateSemester(semester);
+    
+    if (semester.getId() != 0) {
+      throw new ValidationException("Cannot specify semester id when creating new semester");
+    }
+    Semester existing = store.getSemesterBySeasonAndYear(semester.getSeason(), semester.getYear());
+    if (existing != null) {
+      throw new ValidationException("Semester already exists for Season: "+semester.getSeason() +
+        " and Year: "+semester.getYear());
+    }
+  }
+  
+  public void updateSemester(Semester semester) throws ValidationException {
+    validateSemester(semester);
+    
+    if (semester.getId() == 0) {
+      throw new ValidationException("Semester id must be set for update");
+    }
+    Semester existing = store.getSemesterById(semester.getId());
+    if (existing == null) {
+      throw new ValidationException("Cannot update semester since it does not already exist");
+    }
+    Semester seasonYear = store.getSemesterBySeasonAndYear(semester.getSeason(), semester.getYear());
+    if (existing.getId() != seasonYear.getId()) {
+      throw new ValidationException("Semester already exists for Season: "+semester.getSeason() +
+          " and Year: "+semester.getYear());
+    }
+  }
+  
+  private static void validateSemester(Semester semester) throws ValidationException {
+    ValidationException exception = new ValidationException();
+    
+    if (semester == null) {
+      throw new ValidationException("Semester cannot be null");
+    }
+    
+    //Null Checks
+    if (semester.getAdmissionStart() == null) {
+      exception.addMessage("Admission Start Date cannot be null");
+    }
+    if (semester.getAdmissionStop() == null) {
+      exception.addMessage("Admission Stop Date cannot be null");
+    }
+    if (semester.getClassRegistrationDate() == null) {
+      exception.addMessage("Class Registration Date cannot be null");
+    }
+    if (semester.getDropAddDeadlineDate() == null) {
+      exception.addMessage("Drop Add Date cannot be null");
+    }
+    if (semester.getFirstClassDate() == null) {
+      exception.addMessage("First Class Date cannot be null");
+    }
+    if (semester.getFirstExamDate() == null) {
+      exception.addMessage("First Exam Date cannot be null");
+    }
+    if (semester.getLastClassDate() == null) {
+      exception.addMessage("Last Class Date cannot be null");
+    }
+    if (semester.getLastExamDate() == null) {
+      exception.addMessage("Last Exam Date cannot be null");
+    }
+    if (semester.getSeason() == null) {
+      exception.addMessage("Season cannot be null");
+    }
+    if (semester.getYear() < Semester.FIRST_YEAR) {
+      exception.addMessage("Year must be before "+Semester.FIRST_YEAR);
+    }
+    
+    //Date Order Checks
+    if (ValidationUtil.allNotNull(semester.getAdmissionStart(), semester.getAdmissionStop()) &&
+        semester.getAdmissionStop().isBefore(semester.getAdmissionStart())) {
+      exception.addMessage("Admission State Date must be before Admission Stop Date");
+    }
+    if (ValidationUtil.allNotNull(semester.getAdmissionStop(), semester.getClassRegistrationDate()) &&
+        semester.getClassRegistrationDate().isBefore(semester.getAdmissionStop())) {
+      exception.addMessage("Admission Stop Date must be before Class Registration Date");
+    }
+    if (ValidationUtil.allNotNull(semester.getClassRegistrationDate(), semester.getFirstClassDate()) &&
+        semester.getFirstClassDate().isBefore(semester.getClassRegistrationDate())) {
+      exception.addMessage("Class Registration Date must be before First Class Date");
+    }
+    if (ValidationUtil.allNotNull(semester.getFirstClassDate(), semester.getDropAddDeadlineDate()) &&
+        semester.getDropAddDeadlineDate().isBefore(semester.getDropAddDeadlineDate())) {
+      exception.addMessage("First Class Date must be before Drop Add Date");
+    }
+    if (ValidationUtil.allNotNull(semester.getDropAddDeadlineDate(), semester.getLastClassDate()) && 
+        semester.getLastClassDate().isBefore(semester.getDropAddDeadlineDate())) {
+      exception.addMessage("Drop Add Date must be before Last Class Date");
+    }
+    if (ValidationUtil.allNotNull(semester.getLastClassDate(), semester.getFirstExamDate()) && 
+        semester.getLastExamDate().isBefore(semester.getLastClassDate())) {
+      exception.addMessage("Last Class Date must be before First Exam Date");
+    }
+    if (ValidationUtil.allNotNull(semester.getFirstExamDate(), semester.getLastClassDate()) && 
+        semester.getLastExamDate().isBefore(semester.getFirstExamDate())) {
+      exception.addMessage("First Exam Date must be before Last Exam Date");
+    }
+    
+    if (exception.hasErrors()) {
+      throw exception;
+    }
+  }
+
+}
---
+++ liontrail-web/src/main/java/edu/psu/liontrail/store/GradeStore.java
@@ -0,0 +1,46 @@
+package edu.psu.liontrail.store;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import edu.psu.liontrail.enumeration.Grade;
+import edu.psu.liontrail.enumeration.SemesterSeason;
+import edu.psu.liontrail.model.Semester;
+
+@Stateless
+public class GradeStore {
+  
+  @PersistenceContext(unitName = "liontrail-ds")
+  EntityManager em;
+
+  public void createGrade(Grade grade) {
+    em.persist(grade);
+  }
+  
+  public void updateGrade(Grade grade) {
+    em.merge(grade);
+  }
+  
+  public Grade getGradeById(int id) {
+    return em.find(Grade.class, id);
+  }
+  
+  public List<Grade> getGradeBySemester(int id, Semester semester) {
+    // Under Construction
+    // TypedQuery<Grade> query = em.createNamedQuery();
+    // return query.getResultList();
+    return null;
+  }
+  
+  public Grade getGradeByProjection(int id, List<Grade> gradeList) {
+    /*TypedQuery<Grade> query = em.createNamedQuery(Semester.BY_SEASON_AND_YEAR, Semester.class);
+    query.setParameter("season", season);
+    query.setParameter("year", year);
+    return query.getSingleResult();*/
+    return null;
+  }
+}
