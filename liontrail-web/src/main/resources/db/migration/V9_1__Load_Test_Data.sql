1342d3cbb874634d40fd3b6a355240c73da38632
rickyreyna11 <31482626+rickyreyna11@users.noreply.github.com>
2017-10-12 18:14:51 -0500

    Update V9_1__Load_Test_Data.sql
    I just added the /* Major Table: ARTS*/

--- liontrail-web/src/main/resources/db/migration/V9_1__Load_Test_Data.sql
+++ liontrail-web/src/main/resources/db/migration/V9_1__Load_Test_Data.sql
@@ -1,3 +1,4 @@
+/* Semester Table*/
 insert into semester(id,adminssion_start,adminssion_stop,class_registration_date,drop_add_deadline_date,first_class_date,first_exam_date,last_class_date,
 last_exam_date,season,year) values (nextval('semester_id_seq'),'2017-01-01','207-05-31','2017-07-20','2017-09-08','2017-08-21',
 '2017-12-11','2017-12-08','2017-12-15','FALL',2017);
@@ -6,9 +7,13 @@ insert into semester(id,adminssion_start,adminssion_stop,class_registration_date
 last_exam_date,season,year) values (nextval('semester_id_seq'),'2017-06-01','2017-08-31','2017-11-20','2017-01-08','2018-01-22',
 '2018-05-07','2018-05-04','2018-05-11','SPRING',2018);
 
+/* auth_user Table*/
 insert into auth_user(username, password) values('admin','jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=');
+
+/* auth_user_role Table*/
 insert into auth_user_role(username, role) values('admin','admin');
 
+/* Departmement Table*/
 insert into department(id, name) values('ENGINEERING','College of Engineering');
 insert into department(id, name) values('ARTS','College of Arts');
 insert into department(id, name) values('BUSINESS','Smeal College of Business');
@@ -16,6 +21,7 @@ insert into department(id, name) values('MEDICINE','Hershey College of Medicine'
 insert into department(id, name) values('LAW','Dickson Law School');
 insert into department(id, name) values('MATH','College of Math');
 
+/* Major Table: Engineering*/
 insert into major(id, abbreviation, degree_level, name, department_id)
     values(nextval('major_id_seq'), 'SWENG', 'BS', 'Software Engineering', 'ENGINEERING');
 insert into major(id, abbreviation, degree_level, name, department_id)
@@ -25,6 +31,7 @@ insert into major(id, abbreviation, degree_level, name, department_id)
 insert into major(id, abbreviation, degree_level, name, department_id)
     values(nextval('major_id_seq'), 'EE', 'BS', 'Electrical Engineering', 'ENGINEERING');
     
+/* Course Table: Engineering*/    
 insert into course(id, major_id, number, credits, name, description)
 	values(nextval('course_id_seq'), 1, 101, 3, 'Intro to Software Engineering',
 	'This course provides an overview of the discipline of Software Engineering');
@@ -35,6 +42,24 @@ insert into course(id, major_id, number, credits, name, description)
 	values(nextval('course_id_seq'), 1, 210, 3, 'Object Oriented Programming',
 	'This course provides an introduction to Object Oriented Programming');
 	
+/* Major Table: ARTS*/
+insert into major(id, abbreviation, degree_level, name, department_id)
+    values(nextval('major_id_seq'), 'ARTS', 'BA', 'Graphic Design', 'ARTS');
+insert into major(id, abbreviation, degree_level, name, department_id)
+    values(nextval('major_id_seq'), 'ARTS', 'BA', 'Painting', 'ARTS');
+insert into major(id, abbreviation, degree_level, name, department_id)
+    values(nextval('major_id_seq'), 'ARTS', 'BA', 'Sculture', 'ARTS');
+insert into major(id, abbreviation, degree_level, name, department_id)
+    values(nextval('major_id_seq'), 'ARTS', 'MA', 'Teaching Art', 'ARTS');
+
+/* Major Table: Business*/
+
+/* Major Table: Medicine*/
+
+/* Major Table: Law*/
+
+/* Major Table: Math*/
+
 
 insert into course_prerequisites(parent_course, required_course) values(3, 2);
 
