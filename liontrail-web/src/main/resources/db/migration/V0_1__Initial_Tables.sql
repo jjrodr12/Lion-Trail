eac7b4ccc7684c98be4806a81acb3e604b61e6fb
Ricardo <rickyreynapena11@gmail.com>
2017-11-18 13:45:29 -0500

    changes

--- liontrail-web/src/main/resources/db/migration/V0_1__Initial_Tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE some_entity (
-    id character varying(45) NOT NULL,
-    enum_val character varying(15)
-);
-
-ALTER TABLE ONLY some_entity
-    ADD CONSTRAINT some_entity_pkey PRIMARY KEY (id);
